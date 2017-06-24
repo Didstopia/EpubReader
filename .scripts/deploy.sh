e81dcd509b1931c9dbefb3f125d62115b6f25485
Pauli Jokela <pauli.jokela@didstopia.com>
2017-06-24 23:59:29 +0300

    Added automatic NuGet deployment on tags

---
+++ .scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+BUILD_CONFIG=$1
+NUGET_API_KEY=$2
+
+if [[ "$BUILD_CONFIG" == "Release" ]]; then
+	nuget push Didstopia.EpubReader/bin/Release/*.nupkg -ApiKey $NUGET_API_KEY;
+fi
