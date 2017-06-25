03b97c7bc95bdea22e37fe8400d71f966c21b56a
Pauli Jokela <pauli.jokela@didstopia.com>
2017-06-25 08:50:05 +0300

    Fixing Travis

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,6 +7,6 @@ BUILD_CONFIG=$1
 NUGET_API_KEY=$2
 
 if [[ "$BUILD_CONFIG" == "Release" ]]; then
-	nuget push Didstopia.EpubReader/bin/$BUILD_CONFIG/*.nupkg -ApiKey 
-$NUGET_API_KEY;
+	dotnet pack --configuration ${BUILD_CONFIG}
+	nuget push Didstopia.EpubReader/bin/$BUILD_CONFIG/*.nupkg -ApiKey $NUGET_API_KEY;
 fi
